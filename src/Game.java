4d08c7d09f329da95429de706147681cec20b610
Joan Pou Ramon <joanp2001@gmail.com>
2022-06-12 18:31:00 +0200

    Menu

--- src/Game.java
+++ src/Game.java
@@ -1,13 +1,42 @@
-import domino.DominoGame;
-import domino.DominoLatino;
+import domino.*;
 
 public class Game {
     public static void main(String[] args) {
-        newGame();
+        boolean exit = false;
+        do {
+            exit = menu();
+            InputOutput.printLN();
+        } while (exit);
     }
 
-    public static void newGame() {
-        DominoGame domino = new DominoLatino(3,false,100 );
+    public static void newGame(int numberOfPlayers, boolean isTeamGame, int targetPoints) {
+        DominoGame domino = new DominoLatino(numberOfPlayers,isTeamGame,targetPoints );
         domino.gameplay();
+    }
+
+    private static boolean menu() {
+
+        InputOutput.printLN("[1] - Domino Latino (Team)");
+        InputOutput.printLN("[2] - Domino Latino (Single)");
+        InputOutput.printLN("[0] - Exit Game");
+        String menu = InputOutput.input("012");
+
+        switch (menu) {
+            case "1":
+                newGame(4,true,100);
+                break;
+            case "2":
+                InputOutput.printLN("Enter number of players 2-4:");
+                int nPlayers;
+                do {
+                    String s = InputOutput.input("234");
+                    nPlayers = Integer.parseInt(s);
+                } while (nPlayers < 2 || nPlayers > 4);
+                newGame(nPlayers,false,100);
+                break;
+            case "0":
+                return false;
+        }
+        return true;
     }
 }
